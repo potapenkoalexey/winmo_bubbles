39a119850ddef87b10c061f638b14ffbef692ac8
Aleksei Potapenko <potapenkoaleksei@outlook.com>
2020-01-27 12:26:48 +0300

    add an internal Intel CPU random generator instruction (comment)

--- src/engine/random_generator.cxx
+++ src/engine/random_generator.cxx
@@ -2,5 +2,9 @@
 
 std::mt19937& random_generator::getMt19937()
 {
+    /* // internal random generator from Intel CPU
+    unsigned long long ran = 0;
+    __builtin_ia32_rdrand64_step(&ran);
+    */
     return random_generator::instance().mMt;
 }
