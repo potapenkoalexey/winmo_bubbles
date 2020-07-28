c3f623ccb7e1b5b1edd28ece1246e29226a55c2a
Potapenko Aleksei <potapenkoaleksei@outlook.com>
2020-07-28 21:45:58 +0300

    merge fixed

--- src/game/states/classic_state.cxx
+++ src/game/states/classic_state.cxx
@@ -24,11 +24,6 @@ bool classic_state::init(grottans::engine* e)
     game_field = std::make_unique<field>(engine);
     game_field->init();
     game_field->fill_clasic();
-<<<<<<< HEAD
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    //game_field->load_field_from_file();
-=======
->>>>>>> save&restore
 
     ///progress desk
     progress = std::make_unique<progress_desk>(engine);
